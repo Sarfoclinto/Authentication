ccd62e917af4ebca2c58553d02355b330364e04f
Sherlock <wolffighter00000@gmail.com>
2024-05-23 22:17:11 +0100

    all cards complete

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -8,15 +8,25 @@ function Dashboard() {
       <CardNav />
       <div id="content">
         <div id="cards " className="flex w-full gap-6 ">
-          <Card title="Web Development" task={10} value={96} bg="#71357b" />
-          <Card title="Mobile App Design" task={12} value={48} bg="#71357b" />
+          <Card
+            title="Web Development"
+            task={10}
+            value={96}
+            bg="bg-[#71357b]"
+          />
+          <Card
+            title="Mobile App Design"
+            task={12}
+            value={48}
+            bg="bg-orange-500"
+          />
           <Card
             title="Facebook Brand UI Kit"
             task={22}
             value={73}
-            bg="#71357b"
+            bg="bg-blue-700"
           />
-          <Card title="Web Development" task={10} value={96} bg="#71357b" />
+          <Card title="Web Development" task={10} value={96} bg="bg-teal-400" />
         </div>
       </div>
     </div>
